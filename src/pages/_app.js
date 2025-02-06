c287785664e0149638ee46b8f554d2c47c910c65
Gilnathan <gilnathan.dev@icloud.com>
2025-02-06 00:17:22 -0300

    correção bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,6 @@
 import "@/styles/globals.css";
 import "leaflet/dist/leaflet.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <SpeedInsights />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
